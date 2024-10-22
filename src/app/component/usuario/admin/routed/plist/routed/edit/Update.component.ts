6910cd4ac2f5695ac659b9590e9e409c6eb766d2
EloyML05 <eloymitalopez3@gmai.com>
2024-10-22 19:04:29 +0200

    actualizacion del editar que uso de formularios reactivos

--- src/app/component/usuario/admin/routed/plist/routed/edit/Update.component.ts
+++ src/app/component/usuario/admin/routed/plist/routed/edit/Update.component.ts
@@ -2,39 +2,77 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../../../../../../service/usuario.service';
 import { ActivatedRoute } from '@angular/router';
 import { IUsuario } from '../../../../../../../model/usuario.interface';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-Update',
   templateUrl: './Update.component.html',
   styleUrls: ['./Update.component.css'],
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
 })
 export class UpdateComponent implements OnInit {
-id: number = 0
-IUsuario: IUsuario = {
-  id: 0,
-  nombre: '',
-  apellido1: '',
-  apellido2: '',
-  email: '',
-}
+  id: number = 0;
+  IUsuario: IUsuario = {
+    id: 0,
+    nombre: '',
+    apellido1: '',
+    apellido2: '',
+    email: '',
+  };
 
-  constructor( private oUsuarioService: UsuarioService, private oActivatedRoute: ActivatedRoute) { 
+  profileForm: FormGroup = new FormGroup({
+    id: new FormControl(),
+    nombre: new FormControl(),
+    apellido1: new FormControl(),
+    apellido2: new FormControl(),
+    email: new FormControl(),
+  });
+
+  constructor(
+    private oUsuarioService: UsuarioService,
+    private oActivatedRoute: ActivatedRoute
+  ) {
     this.id = this.oActivatedRoute.snapshot.params['id'];
   }
 
   ngOnInit() {
-
     this.oUsuarioService.getUser(this.id).subscribe({
       next: (data) => {
         this.IUsuario = data;
+        this.profileForm = new FormGroup({
+          id: new FormControl(this.IUsuario?.id, [Validators.required]),
+          nombre: new FormControl(this.IUsuario?.nombre, [Validators.required]),
+          apellido1: new FormControl(this.IUsuario?.apellido1, [
+            Validators.required,
+          ]),
+          apellido2: new FormControl(this.IUsuario?.apellido2, [
+            Validators.required,
+          ]),
+          email: new FormControl(this.IUsuario?.email, [Validators.required]),
+        });
       },
       error: (err) => {
         console.log(err);
       },
-    })
+    });
+  }
+
+  onSubmit() {
+    this.IUsuario.id = this.profileForm?.value.id;
+    this.IUsuario.nombre = this.profileForm?.value.nombre;
+    this.IUsuario.apellido1 = this.profileForm?.value.apellido1;
+    this.IUsuario.apellido2 = this.profileForm?.value.apellido2;
+    this.IUsuario.email = this.profileForm?.value.email;
+
+    this.update();
   }
 
   update() {
@@ -46,7 +84,6 @@ IUsuario: IUsuario = {
       error: (err) => {
         console.log(err);
       },
-    })
+    });
   }
-
 }
